dc6f8068cec57708098c978e22304ec68730437a
Patrick Vollebregt <patrick@crosscode.nl>
2022-11-29 00:13:34 +0100

    Implemented potential cert decoder

---
+++ src/main/java/nl/crosscode/x509certbuddy/CertRetriever.java
@@ -0,0 +1,66 @@
+package nl.crosscode.x509certbuddy;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CertRetriever {
+    CertificateFactory certFactory;
+
+    public CertRetriever() throws CertificateException {
+        certFactory = CertificateFactory.getInstance("X.509");
+    }
+
+    public List<X509Certificate> retrieveCerts(String text) throws CertificateException {
+        List<X509Certificate> certificates = new ArrayList<X509Certificate>();
+        for (byte[] potentialCert : findPotentialCerts(text)) {
+            try {
+                certificates.add(certFromBytes(potentialCert));
+            } catch (Exception e) {} // Ignoring it for now due to the brute force nature of cert finding.
+        }
+        return certificates;
+    }
+
+    private X509Certificate certFromBytes(byte[] bytes) throws CertificateException {
+        InputStream in = new ByteArrayInputStream(bytes);
+        return (X509Certificate)certFactory.generateCertificate(in);
+    }
+
+    private List<byte[]> findPotentialCerts(String text) {
+        List<byte[]> potentialCerts = new ArrayList<byte[]>();
+        List<Decoder> decoders = new ArrayList<Decoder>();
+        for (char c : text.toCharArray()) {
+            if (c=='M') {
+                decoders.add(new Decoder());
+            }
+            for (Decoder decoder : decoders) {
+                if (decoder.add(c)) { // decoder is certainly done
+                    decodeToPotentialCerts(potentialCerts, decoder);
+                }
+            }
+            decoders = decoders.stream().filter(decoder -> !decoder.isDone()).collect(Collectors.toList());
+        }
+        for (Decoder decoder : decoders) {
+            decodeToPotentialCerts(potentialCerts, decoder);
+        }
+        return potentialCerts;
+    }
+
+    private static void decodeToPotentialCerts(List<byte[]> potentialCerts, Decoder decoder) {
+        SeqDerReader seqDerReader = new SeqDerReader();
+        byte[] data = decoder.tryDecode();
+        for (byte b : data) {
+            seqDerReader.read(b);
+        }
+        seqDerReader.setEof();
+        if (!seqDerReader.isError()) {
+            potentialCerts.add(seqDerReader.getResult());
+        }
+    }
+}
---
+++ src/main/java/nl/crosscode/x509certbuddy/Decoder.java
@@ -0,0 +1,32 @@
+package nl.crosscode.x509certbuddy;
+
+import java.util.Arrays;
+import java.util.Base64;
+
+public class Decoder {
+    private final String base64Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    private String data;
+    private Boolean readPadding = false;
+    private Boolean done = false;
+
+    public Boolean add(char c) {
+        // TODO: Alternative alphabets
+        if (done) return true;
+        if (base64Alphabet.indexOf(c)==-1) return false;
+        if (readPadding && c!='=') {
+            done = true;
+            return true;
+        }
+        if (c=='=') readPadding = true;
+        data+=c;
+        return false;
+    }
+
+    public byte[] tryDecode() {
+        return Base64.getDecoder().decode(data);
+    }
+
+    Boolean isDone() {
+        return done;
+    }
+}
