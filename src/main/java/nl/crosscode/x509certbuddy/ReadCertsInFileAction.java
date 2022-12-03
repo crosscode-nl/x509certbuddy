3e73e247afeea859afffadd7b479009e709de8b3
Patrick Vollebregt <patrick@crosscode.nl>
2022-12-03 01:46:11 +0100

    Add inlay

--- src/main/java/nl/crosscode/x509certbuddy/ReadCertsInFileAction.java
+++ src/main/java/nl/crosscode/x509certbuddy/ReadCertsInFileAction.java
@@ -5,6 +5,13 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DataKey;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.EditorCustomElementRenderer;
+import com.intellij.openapi.editor.Inlay;
+import com.intellij.openapi.editor.colors.EditorColors;
+import com.intellij.openapi.editor.markup.HighlighterLayer;
+import com.intellij.openapi.editor.markup.HighlighterTargetArea;
+import com.intellij.openapi.editor.markup.RangeHighlighter;
+import com.intellij.openapi.editor.markup.TextAttributes;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
@@ -12,6 +19,8 @@ import com.intellij.openapi.wm.ToolWindowManager;
 import org.bouncycastle.mime.encoding.Base64InputStream;
 import org.jetbrains.annotations.NotNull;
 
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -27,6 +36,31 @@ public class ReadCertsInFileAction extends AnAction {
         Project project = e.getProject();
         if (project==null) return;
         Editor editor = FileEditorManager.getInstance(project).getSelectedTextEditor();
+        /*
+        Inlay<EditorCustomElementRenderer> hello = editor.getInlayModel().addBlockElement(00, false, true, 0, new EditorCustomElementRenderer() {
+            @Override
+            public int calcWidthInPixels(@NotNull Inlay inlay) {
+                return 200;
+            }
+
+            @Override
+            public int calcHeightInPixels(@NotNull Inlay inlay) {
+                return 200;
+            }
+
+            @Override
+            public void paint(@NotNull Inlay inlay, @NotNull Graphics2D g, @NotNull Rectangle2D targetRegion, @NotNull TextAttributes textAttributes) {
+                g.drawString("Hello", (int) targetRegion.getX(), (int) targetRegion.getMaxY());
+                EditorCustomElementRenderer.super.paint(inlay, g, targetRegion, textAttributes);
+            }
+        });
+
+        hello.dispose();
+        */
+        //  TextAttributes color = editor.getColorsScheme().getAttributes(EditorColors.SEARCH_RESULT_ATTRIBUTES);
+    //    RangeHighlighter rh = editor.getMarkupModel().addRangeHighlighter(10,20, HighlighterLayer.ADDITIONAL_SYNTAX+1,color,HighlighterTargetArea.EXACT_RANGE);
+//        editor.getMarkupModel().removeHighlighter(rh);
+
         if (editor==null) return;
         String allText = editor.getDocument().getText();
         try {
