e90cf78d92abf0faf5d78225d878965b6b74abdc
Patrick Vollebregt <patrick@crosscode.nl>
2022-12-05 01:51:12 +0100

    Gradle update

--- src/main/java/nl/crosscode/x509certbuddy/x509CertAssistantFactory.java
+++ src/main/java/nl/crosscode/x509certbuddy/x509CertAssistantFactory.java
@@ -4,7 +4,6 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.ui.content.Content;
-import com.intellij.ui.content.ContentFactory;
 import org.jetbrains.annotations.NotNull;
 
 public class x509CertAssistantFactory implements ToolWindowFactory {
