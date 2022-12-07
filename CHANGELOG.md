f592aac8946d4d5dbe6c553a9c33aa5199e128d5
Patrick Vollebregt <patrick@crosscode.nl>
2022-12-07 21:59:48 +0100

    Ui improvements

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,5 +5,7 @@
 * Parse certificates with escaped linefeeds in json strings
 * No longer remove certs when reading them from a document
 * Remove duplicate certificates from the tree
+* UI improvements
+* Allow removal of single or all certificates from the tree
 
 # 0.1
--- src/main/java/nl/crosscode/x509certbuddy/ui/x509CertAssistant.java
+++ src/main/java/nl/crosscode/x509certbuddy/ui/x509CertAssistant.java
@@ -5,17 +5,16 @@ import nl.crosscode.x509certbuddy.wrappers.X509CertWrapper;
 import nl.crosscode.x509certbuddy.utils.X509Utils;
 import nl.crosscode.x509certbuddy.wrappers.HexDumpWrapper;
 import nl.crosscode.x509certbuddy.wrappers.OpenSslWrapper;
+import org.bouncycastle.jcajce.provider.asymmetric.X509;
 
 import javax.swing.*;
 import javax.swing.event.TreeSelectionEvent;
-import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
 import java.awt.*;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.security.cert.CertificateEncodingException;
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
@@ -28,78 +27,96 @@ public class x509CertAssistant {
     private static final Logger log = Logger.getInstance(x509CertAssistant.class);
     private String pemString;
     private String base64String;
-
-    public x509CertAssistant() {
-        log.warn("Constructed x509CertAssistant");
-        DefaultTreeModel model = (DefaultTreeModel)certTree.getModel();
-        DefaultMutableTreeNode root = new DefaultMutableTreeNode("Certs",true);
-        model.setRoot(root);
-        model.reload();
-        certTree.addTreeSelectionListener(new TreeSelectionListener() {
-            @Override
-            public void valueChanged(TreeSelectionEvent e) {
-                certDetails.setText("");
-                DefaultMutableTreeNode treeNode = (DefaultMutableTreeNode)certTree.getLastSelectedPathComponent();
-                if (treeNode==null) return;
-                X509Certificate cert = ((X509CertWrapper) treeNode.getUserObject()).getCert();
-                if (cert==null) return;
-                String details = OpenSslWrapper.getCertDetails(cert);
-                if (details==null) {
-                    details = cert.toString();
-                }
-                certDetails.setText(details);
-                certDetails.setCaretPosition(0);
-                pemString = OpenSslWrapper.getPem(cert);
-                pem.setText(pemString);
-                base64String = X509Utils.getBase64(cert);
-                asn1.setText(OpenSslWrapper.getAsn1(cert));
-                hex.setText(HexDumpWrapper.getHex(cert));
-                validation.setText(OpenSslWrapper.getValidation(cert,x509Certificates));
-            }
-        });
-
-        copyPem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (pemString==null) return;
-                Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-                StringSelection ss = new StringSelection(pemString);
-                clipboard.setContents(ss,ss);
-            }
-        });
-
-        copyBase64.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (base64String==null) return;
-                Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-                StringSelection ss = new StringSelection(base64String);
-                clipboard.setContents(ss,ss);
-            }
-        });
-
-
-    }
-
-
     private List<X509Certificate> x509Certificates = new ArrayList<>();
-
+    private X509Certificate selectedCertificate = null;
     private JPanel rootPanel;
     private JTree certTree;
     private JTabbedPane tabbedPane1;
-    private JTextPane certDetails;
-    private JTextPane pem;
-    private JButton copyPem;
-    private JButton copyBase64;
-    private JTextPane asn1;
-    private JTextPane hex;
-    private JTextPane validation;
+    private JTextPane certDetailsTextPane;
+    private JTextPane pemTextPane;
+    private JButton copyPEMButton;
+    private JTextPane asn1TextPane;
+    private JTextPane hexTextPane;
+    private JTextPane validationTextPane;
+    private JButton removeCertButton;
+    private JButton clearButton;
+    private JButton copyBase64Button;
+    public x509CertAssistant() {
+        log.warn("Constructed x509CertAssistant");
+        DefaultTreeModel model = (DefaultTreeModel) certTree.getModel();
+        DefaultMutableTreeNode root = new DefaultMutableTreeNode("Certs", true);
+        model.setRoot(root);
+        model.reload();
+        certTree.addTreeSelectionListener(this::treeSelectionChanged);
+        copyPEMButton.addActionListener(this::copyPem);
+        copyBase64Button.addActionListener(this::copyBase64);
+        removeCertButton.addActionListener(this::removeCert);
+        clearButton.addActionListener(this::clear);
+    }
+
+    private void treeSelectionChanged(TreeSelectionEvent e) {
+        certDetailsTextPane.setText("");
+        DefaultMutableTreeNode treeNode = (DefaultMutableTreeNode) certTree.getLastSelectedPathComponent();
+        if (treeNode == null) {
+            selectedCertificate = null;
+            itemSelected(false);
+            return;
+        }
+        itemSelected(true);
+        selectedCertificate = ((X509CertWrapper) treeNode.getUserObject()).getCert();
+        updateFieldsWithCertDetails();
+    }
+
+    private void updateFieldsWithCertDetails() {
+        if (selectedCertificate == null) return;
+        String details = OpenSslWrapper.getCertDetails(selectedCertificate);
+        if (details == null) {
+            details = selectedCertificate.toString();
+        }
+        certDetailsTextPane.setText(details);
+        certDetailsTextPane.setCaretPosition(0);
+        pemString = OpenSslWrapper.getPem(selectedCertificate);
+        pemTextPane.setText(pemString);
+        base64String = X509Utils.getBase64(selectedCertificate);
+        asn1TextPane.setText(OpenSslWrapper.getAsn1(selectedCertificate));
+        hexTextPane.setText(HexDumpWrapper.getHex(selectedCertificate));
+        validationTextPane.setText(OpenSslWrapper.getValidation(selectedCertificate, x509Certificates));
+    }
+
+    private void itemSelected(boolean value) {
+        copyBase64Button.setEnabled(value);
+        copyPEMButton.setEnabled(value);
+        removeCertButton.setEnabled(value);
+    }
+
+    private void removeCert(ActionEvent e) {
+        x509Certificates.remove(selectedCertificate);
+        buildTree();
+    }
+
+    private void clear(ActionEvent e) {
+        x509Certificates.clear();
+        buildTree();
+    }
+
+    private void copyPem(ActionEvent e) {
+        if (pemString == null) return;
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        StringSelection ss = new StringSelection(pemString);
+        clipboard.setContents(ss, ss);
+    }
+
+    private void copyBase64(ActionEvent e) {
+        if (base64String == null) return;
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        StringSelection ss = new StringSelection(base64String);
+        clipboard.setContents(ss, ss);
+    }
 
     public JPanel getContent() {
         log.warn("getContent is called");
         return rootPanel;
     }
-
 
     public void addCerts(List<X509Certificate> certs) {
         log.warn("Adding certs");
@@ -111,51 +128,52 @@ public class x509CertAssistant {
     private void removeDuplicateCerts() {
         List<X509Certificate> certs = new ArrayList<>();
         for (X509Certificate cert : x509Certificates) {
-           if (certs.stream().anyMatch(x-> {
-               try {
-                   return Arrays.equals(x.getEncoded(),cert.getEncoded());
-               } catch (CertificateEncodingException e) {
-                   return false;
-               }
-           })) continue;
-           certs.add(cert);
+            if (certs.stream().anyMatch(x -> {
+                try {
+                    return Arrays.equals(x.getEncoded(), cert.getEncoded());
+                } catch (CertificateEncodingException e) {
+                    return false;
+                }
+            })) continue;
+            certs.add(cert);
         }
         x509Certificates = certs;
     }
 
     private void buildTree() {
-        DefaultTreeModel model = (DefaultTreeModel)certTree.getModel();
-        DefaultMutableTreeNode root = new DefaultMutableTreeNode("Certs",true);
+        selectedCertificate = null;
+        DefaultTreeModel model = (DefaultTreeModel) certTree.getModel();
+        DefaultMutableTreeNode root = new DefaultMutableTreeNode("Certs", true);
         root.removeAllChildren();
-        List<X509Certificate> rootCerts = x509Certificates.stream().filter(x-> !x509Certificates.stream().anyMatch(y->y.getSubjectDN().getName().equals(x.getIssuerDN().getName())&&x!=y)).collect(Collectors.toList());
+        List<X509Certificate> rootCerts = x509Certificates.stream().filter(x -> !x509Certificates.stream().anyMatch(y -> y.getSubjectDN().getName().equals(x.getIssuerDN().getName()) && x != y)).collect(Collectors.toList());
         for (X509Certificate cert : rootCerts) {
             log.warn(cert.getSubjectDN().getName());
             log.warn(cert.getIssuerDN().getName());
             DefaultMutableTreeNode treeNode = new DefaultMutableTreeNode(new X509CertWrapper(cert));
-        //    treeNode.setUserObject(cert);
-            addChildren(treeNode,cert,x509Certificates);
+            //    treeNode.setUserObject(cert);
+            addChildren(treeNode, cert, x509Certificates);
             root.add(treeNode);
         }
         model.setRoot(root);
         model.reload();
-        expandAllNodes(certTree,0,0);
+        expandAllNodes(certTree, 0, 0);
     }
 
-    private void expandAllNodes(JTree tree, int startingIndex, int rowCount){
-        for(int i=startingIndex;i<rowCount;++i){
+    private void expandAllNodes(JTree tree, int startingIndex, int rowCount) {
+        for (int i = startingIndex; i < rowCount; ++i) {
             tree.expandRow(i);
         }
 
-        if(tree.getRowCount()!=rowCount){
+        if (tree.getRowCount() != rowCount) {
             expandAllNodes(tree, rowCount, tree.getRowCount());
         }
     }
 
     public void addChildren(DefaultMutableTreeNode treeNode, X509Certificate cert, List<X509Certificate> certs) {
-        List<X509Certificate> children = certs.stream().filter(x->x.getIssuerDN().getName().equals(cert.getSubjectDN().getName())&&x!=cert).collect(Collectors.toList());
+        List<X509Certificate> children = certs.stream().filter(x -> x.getIssuerDN().getName().equals(cert.getSubjectDN().getName()) && x != cert).collect(Collectors.toList());
         for (X509Certificate child : children) {
             DefaultMutableTreeNode childNode = new DefaultMutableTreeNode(new X509CertWrapper(child));
-            addChildren(childNode,child,certs);
+            addChildren(childNode, child, certs);
             treeNode.add(childNode);
         }
     }
