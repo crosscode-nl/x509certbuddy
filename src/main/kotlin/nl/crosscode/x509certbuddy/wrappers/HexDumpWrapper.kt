dc23e320e972c904710eae8d311b0188b7b82c77
Patrick Vollebregt <patrick@crosscode.nl>
2024-04-17 21:22:12 +0200

    feat: directly generate hexdump from code

--- src/main/kotlin/nl/crosscode/x509certbuddy/wrappers/HexDumpWrapper.kt
+++ src/main/kotlin/nl/crosscode/x509certbuddy/wrappers/HexDumpWrapper.kt
@@ -3,6 +3,36 @@ package nl.crosscode.x509certbuddy.wrappers
 
 import java.security.cert.X509Certificate
 
-fun getHex(cert: X509Certificate): String? {
-    return performCommand(cert, "hexdump", "-C")
+// getHex returns the hexdump of the certificate
+00000000  30 82 05 D2 30 82 04 BA  A0 03 02 01 02 02 10 08  |0...0...........|
+fun getHex(cert: X509Certificate): String {
+    val address = 0 // Start address
+    val data = cert.encoded
+    val length = data.size
+    val hex = StringBuilder()
+    val ascii = StringBuilder()
+    var i = 0
+    while (i < length) {
+        hex.append(String.format("%08X  ", address + i))
+        var j = 0
+        while (j < 16) {
+            if (i + j < length) {
+                val b = data[i + j].toInt() and 0xFF
+                hex.append(String.format("%02X ", b))
+                ascii.append(if (b in 32..126) b.toChar() else '.')
+            } else {
+                hex.append("   ")
+            }
+            if (j == 7) {
+                hex.append(" ")
+            }
+            j++
+        }
+        hex.append(" |")
+        hex.append(ascii)
+        hex.append("|\n")
+        ascii.setLength(0)
+        i += 16
+    }
+    return hex.toString()
 }
